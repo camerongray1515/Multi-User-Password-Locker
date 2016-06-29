02a7c5caab8faa25103982efb8aabe269b49a8c5
Cameron Gray <cameron@camerongray.me>
2016-06-29 23:17:40 +0100

    Setting folder permissions complete.  Client API methods now deal with objects rather than passing dictionaries around

--- tui_client/src/locker.py
+++ tui_client/src/locker.py
@@ -9,6 +9,35 @@ class RequestFailedError(Exception):
         super(RequestFailedError, self).__init__(message)
         self.error_type = error_type
 
+class LockerEntity():
+    def to_dict(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return str(self.to_dict())
+
+class FolderPermission(LockerEntity):
+    def __init__(self, user_id, read, write):
+        self.user_id = int(user_id)
+        self.read = bool(read)
+        self.write = bool(write)
+
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "read": self.read,
+            "write": self.write
+        }
+
+class Folder(LockerEntity):
+    def __init__(self, name, folder_id=None):
+        self.id = folder_id
+        self.name = name
+
+    def to_dict(self):
+        return {"name": self.name, "id": self.id}
+
+
 class Locker:
     def __init__(self, server, port, username, password):
         self.server = server
@@ -35,15 +64,39 @@ class Locker:
 
     def get_folders(self):
         r = requests.get(self._get_url("folders"), auth=self._get_auth()).json()
-        return r["folders"]
 
-    def add_folder(self, name):
-        r = requests.put(self._get_url("folders/add"), json={"name": name},
+        folders = []
+
+        for f in r["folders"]:
+            folders.append(Folder(f["name"], f["id"]))
+
+        return folders
+
+    def add_folder(self, folder):
+        r = requests.put(self._get_url("folders/add"), json=folder.to_dict(),
             auth=self._get_auth()).json()
 
         self._check_errors(r)
 
-        return r.get("folder_id")
+        folder.id = r.get("folder_id")
+
+        return folder
+
+    def set_folder_permissions(self, folder_id, permissions):
+        if type(permissions) == FolderPermission:
+            permissions = [permissions]
+
+        permission_dicts = []
+        for p in permissions:
+            permission_dicts.append(p.to_dict())
+
+        r = requests.post(self._get_url("folders/set_permissions"),
+            json={"folder_id": folder_id, "permissions": permission_dicts},
+            auth=self._get_auth()).json()
+
+        self._check_errors(r)
+
+        return True
 
     def delete_folder(self, folder_id):
         r = requests.delete(
@@ -57,7 +110,9 @@ class Locker:
 if __name__ == "__main__":
     l = Locker("127.0.0.1", 5000, "camerongray", "password")
     try:
-        print(l.delete_folder(2))
+        # p = FolderPermission(user_id=1, read=True, write=True)
+        # l.set_folder_permissions(1, p)
+        print(l.add_folder(Folder("Second test folder")).to_dict())
     except RequestFailedError as ex:
         print(ex.error_type)
         print(str(ex))
